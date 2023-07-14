736f092d81a819e24b2119023cb0c94ba8d170ac
Ananyahs <39176552+Ananyahs@users.noreply.github.com>
2023-07-14 16:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let str=prompt("enter a string");
+let freq=[];
+for(let i=0;i<freq.length;i++)
+	{
+		if(freq[str[i]])
+		{
+			freq[str[i]]++;
+		}
+		else{
+			freq[str[i]]=1;
+		}
+	}
+for(let i=0;i<str.length;i++)
+	{
+		if(freq[str[i]]==1)
+		{
+			alert(str[i]);
+			break;
+		}
+	}
